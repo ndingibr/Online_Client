77056d0b3dce2255970d0d1c8f0f3d282376b3bc
Brian Ndingindwayo <ndingibr@gmail.com>
2016-10-08 13:04:22 +0200

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name onlineClientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the onlineClientApp
+ */
+angular.module('onlineClientApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name onlineClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the onlineClientApp
+ */
+angular.module('onlineClientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
